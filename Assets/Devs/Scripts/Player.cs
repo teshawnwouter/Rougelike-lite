4501fa533d162ce6eb2265ec7ee3d14170597f7f
teshawnwouter <145546485+teshawnwouter@users.noreply.github.com>
2025-01-07 23:47:18 +0100

    Movement done and groundcheck finished

--- Assets/Devs/Scripts/Player.cs
+++ Assets/Devs/Scripts/Player.cs
@@ -7,8 +7,15 @@ public class Player : MonoBehaviour
     public InventoryObjects inventory;
 
     [Header("movement")]
-    private Vector2 moveInputsVectors = Vector2.zero;
-    [SerializeField] private float moveSpeed = 2.5f;
+    private float moveSpeed = 2.5f;
+    private float forwardMovement;
+    private float jumpHeight = 10;
+
+    [Header("grounding")]
+    [SerializeField] private LayerMask groundlayer;
+    [SerializeField] private Transform groundCheck;
+
+    [Header("refrences")]
     private Rigidbody2D rb;
     private void Start()
     {
@@ -17,11 +24,22 @@ public class Player : MonoBehaviour
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        forwardMovement = context.ReadValue<Vector2>().x;
+    }
+    private void Update()
+    {
+        rb.velocity = new Vector2(forwardMovement * moveSpeed,rb.velocity.y);
+    }
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if(context.performed && WhatIsGround())
         {
-            moveInputsVectors = context.ReadValue<Vector2>();
-            rb.velocity = moveInputsVectors * moveSpeed;
+            rb.velocity += Vector2.up * jumpHeight;
         }
+    }
+    private bool WhatIsGround()
+    {
+        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(0.65f, 0.01f), CapsuleDirection2D.Horizontal,0,groundlayer);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
